8bbe9bce4a9c96e900ff481ebabcb91b5c62580e
Jamie Vilchez Giraldo <yamivritra@gmail.com>
2026-03-25 16:54:45 -0500

    first commit

---
+++ src/main/java/com/madness/ygoperu_market/YgoperuMarketApplication.java
@@ -0,0 +1,13 @@
+package com.madness.ygoperu_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YgoperuMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YgoperuMarketApplication.class, args);
+	}
+
+}
